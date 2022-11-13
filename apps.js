bde319c4dcf866ef288fe1965260a1a7a5b42284
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-13 18:07:39 +0600

    fouth commit

--- apps.js
+++ apps.js
@@ -1,13 +1,19 @@
 const API_KEY = `3d3644783ce73409df85a9996d6d5494`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     // console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperatures(data))
 };
 
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
 const displayTemperatures = temperature => {
     console.log(temperature);
+    setInnerText('city', temperature.name);
+
 }
