e30fe25f8ae7c3e92b665db02d5871172451a1f5
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-13 15:24:08 +0600

    third commit

--- apps.js
+++ apps.js
@@ -1,6 +1,13 @@
 const API_KEY = `3d3644783ce73409df85a9996d6d5494`;
 const searchTemperature = () => {
-    const city  = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API%20key}`
-    console.log(url);
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperatures(data))
+};
+
+const displayTemperatures = temperature => {
+    console.log(temperature);
 }
